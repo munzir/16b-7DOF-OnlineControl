06afb7af66981f2cc49d914720638e8f1fa6cf3f
mouhyemen <mouhyemen.khan@gmail.com>
2018-04-30 13:37:44 -0400

    fixed errors, optimized code, predicting torques

--- Controller.cpp
+++ Controller.cpp
@@ -51,11 +51,14 @@ Controller::Controller(dart::dynamics::SkeletonPtr _robot,
     _robot->getJoint(i)->setDampingCoefficient(0, 0.5);
 
   // // Dump data
-  // dataQ.open      ("./data/dataQ.txt");
-  // dataQ       << "dataQ" << endl;
+  dataTime.open   ("./data/dataTime.txt");
+  dataTime    << "dataTime" << endl;
+
+  dataQ.open      ("./data/dataQ.txt");
+  dataQ       << "dataQ" << endl;
   //
-  // dataQref.open   ("./data/dataQref.txt");
-  // dataQref    << "dataQref" << endl;
+  dataQref.open   ("./data/dataQref.txt");
+  dataQref    << "dataQref" << endl;
   //
   // dataQdot.open   ("./data/dataQdot.txt");
   // dataQdot    << "dataQdot" << endl;
@@ -65,9 +68,6 @@ Controller::Controller(dart::dynamics::SkeletonPtr _robot,
   //
   // dataTorque.open ("./data/dataTorque.txt");
   // dataTorque  << "dataTorque" << endl;
-  //
-  // dataTime.open   ("./data/dataTime.txt");
-  // dataTime    << "dataTime" << endl;
   //
   // dataM.open      ("./data/dataM.txt");
   // dataM       << "dataM" << endl;
@@ -137,53 +137,55 @@ Controller::Controller(dart::dynamics::SkeletonPtr _robot,
   }
   cout << "Operation completed!" << endl;
 
-
-  cout << " -------------- " << endl;
-  cout << "Generating optical trajectories ====> ";
+  dt = 0.001;
 
   // Define coefficients for sinusoidal, pulsation frequency for q and dq
-  dt = 0.001;
-  wf = 0.558048373585;
-  Eigen::Matrix<double, DOF, 4> a, b;
-  a << -0.009, -0.36, 0.311, -0.362,
-        0.095, -0.132, -0.363, 0.474,
-        -0.418, -0.25, -0.12, 0.119,
-        0.023, 0.113, 0.497, 0.213,
-        -0.23, -0.237, 0.153, -0.147,
-        0.366, 0.366, 0.302, -0.373,
-        -0.247, -0.166, 0.315, 0.031;
+  cout << " -------------- " << endl;
+  cout << " Setting coefficients for optical trajectories | ";
 
-  b <<  -0.051, 0.027, 0.003, -0.332,
-        -0.292, 0.358, -0.056, -0.436,
-        -0.355, 0.039, -0.397, -0.445,
-        0.328, 0.256, -0.36, 0.143,
-        0.428, 0.093, 0.035, -0.28,
-        -0.39, -0.085, 0.388, 0.46,
-        -0.046, 0.135, -0.428, 0.387;
+  // // Values from actual URDF
+  // wf = 0.558048373585;
+  // a << -0.009, -0.36, 0.311, -0.362,
+  //       0.095, -0.132, -0.363, 0.474,
+  //       -0.418, -0.25, -0.12, 0.119,
+  //       0.023, 0.113, 0.497, 0.213,
+  //       -0.23, -0.237, 0.153, -0.147,
+  //       0.366, 0.366, 0.302, -0.373,
+  //       -0.247, -0.166, 0.315, 0.031;
+  //
+  // b <<  -0.051, 0.027, 0.003, -0.332,
+  //       -0.292, 0.358, -0.056, -0.436,
+  //       -0.355, 0.039, -0.397, -0.445,
+  //       0.328, 0.256, -0.36, 0.143,
+  //       0.428, 0.093, 0.035, -0.28,
+  //       -0.39, -0.085, 0.388, 0.46,
+  //       -0.046, 0.135, -0.428, 0.387;
+  //
+  // q0 << 0.235, -0.004, -0.071, 0.095, -0.141, 0.208, -0.182;
 
-  Eigen::MatrixXd q0(DOF, 1);
-  q0 << 0.235, -0.004, -0.071, 0.095, -0.141, 0.208, -0.182;
+  // Values from random URDF
+  wf = 0.933881552676;
+  a << 0.127, -0.062, 0.464, -0.109,
+      -0.122, -0.447, -0.325, 0.417,
+      -0.156, -0.345, -0.035, 0.436,
+      -0.153, -0.187, 0.181, 0.231,
+      -0.482, -0.377, 0.107, 0.138,
+      0.074, -0.117, 0.403, -0.085,
+      -0.417, 0.357, -0.354, 0.346;
 
-  // Compute joint angles & velocities using Pulsed Trajectories
-  // Eigen::MatrixXd qref(DOF, 1);
-  // Eigen::MatrixXd dqref(DOF, 1);
-  qref << 0, 0, 0, 0, 0, 0, 0;
-  dqref = qref;
+  b << 0.42, -0.485, 0.196, -0.337,
+      -0.394, 0.313, -0.357, -0.363,
+      0.326, -0.278, -0.258, 0.066,
+      -0.475, -0.296, -0.429, 0.11,
+      -0.098, 0.291, -0.137, 0.183,
+      -0.065, 0.069, 0.284, -0.174,
+      -0.434, -0.364, 0.168, -0.001;
 
-  // cout << "Time: " << mTime << endl;
-  for (int joint = 0; joint < DOF; joint++) {
-    for (int l = 1; l <= 4; l++) {
-      qref(joint) = qref(joint) + (a(joint, l-1)/(wf*l))*sin(wf*l*mTime)
-      - (b(joint, l-1)/(wf*l))*cos(wf*l*mTime);
-      dqref(joint) = dqref(joint) + a(joint,l-1)*cos(wf*l*mTime)
-      + b(joint, l-1)*sin(wf*l*mTime);
-    }
-  }
-  qref = qref + q0;
-  // cout << "Angles: " << endl << qref << endl << endl;
-  // cout << "Velocities: " << endl << dqref << endl << endl;
-  cout << "Trajectory generation successful!" << endl << endl;
+  q0 << -0.203, -0.178, 0.097, 0.105, 0.193, -0.274, -0.204;
 
+  mRobot->setPositions(q0);
+
+  cout << "Operation completed!" << endl;
 
   cout << endl << "============= Controller constructor successful ============" << endl << endl;
 }
@@ -261,6 +263,21 @@ Eigen::MatrixXd getQueryKernel(Eigen::MatrixXd xp, Eigen::MatrixXd xq,
 void Controller::update(const Eigen::Vector3d& _targetPosition) {
   // const int dof = mRobot->getNumDofs();
 
+  // Compute joint angles & velocities using Pulsed Trajectories
+  qref << 0, 0, 0, 0, 0, 0, 0;
+  dqref = qref;
+
+  // cout << "Time: " << mTime << endl;
+  for (int joint = 0; joint < DOF; joint++) {
+    for (int l = 1; l <= 4; l++) {
+      qref(joint) = qref(joint) + (a(joint, l-1)/(wf*l))*sin(wf*l*mTime)
+      - (b(joint, l-1)/(wf*l))*cos(wf*l*mTime);
+      dqref(joint) = dqref(joint) + a(joint,l-1)*cos(wf*l*mTime)
+      + b(joint, l-1)*sin(wf*l*mTime);
+    }
+  }
+  qref = qref + q0;
+
   mTime += dt;
 
   // Get the stuff that we need
@@ -295,8 +312,8 @@ void Controller::update(const Eigen::Vector3d& _targetPosition) {
 
 
   // Computing predicted torque
-  const double theta1 = 6.5;
-  const double theta2 = 1.2;
+  const double theta1 = 7.3422;
+  const double theta2 = 11.8002;
   Eigen::Matrix<double, 1, DOF*3> xq;
 
   // xp << q, dq, ddq;
@@ -306,52 +323,56 @@ void Controller::update(const Eigen::Vector3d& _targetPosition) {
   // cout <<"k size: " << k.rows() <<"x"<<   k.cols()   << endl;
   // cout <<"k: " << k << endl << endl;
 
-  //torques
-  mForces = M*ddqRef + Cg;
-  // cout << "Torque (before):" << endl << mForces << endl << endl;
-  Eigen::Matrix<double, DOF, DOF> errCoeff = Eigen::Matrix<double, DOF, DOF>::Identity();
-  errCoeff(0,0) =  30.0;
-  errCoeff(1,1) = 200.0;
-  errCoeff(2,2) =  15.0;
-  errCoeff(3,3) = 100.0;
-  errCoeff(4,4) =   3.0;
-  errCoeff(5,5) =  25.0;
-  errCoeff(6,6) =   1.0;
+  // Torques - reference, predicted
+  // mTauPred  = M*ddq + Cg + (k.transpose()*alpha).transpose();
+  mTauPred  = M*ddqRef + Cg + (k.transpose()*alpha).transpose();
+  mForces   = M*ddqRef + Cg;
 
-  // cout << "Torque (before):" << endl << mForces << endl << endl;
-  Eigen::VectorXd mForceErr = mForces + errCoeff*error(dq, DOF);
-  // cout << "Error:"  << endl << errCoeff*error(dq, DOF) << endl << endl;
-  // cout << "Torque (after):" << endl << mForceErr << endl << endl;
+
+  Eigen::Matrix<double, DOF, DOF> errCoeff = Eigen::Matrix<double, DOF, DOF>::Identity();
+    errCoeff(0,0) =  30.0;
+    errCoeff(1,1) = 200.0;
+    errCoeff(2,2) =  15.0;
+    errCoeff(3,3) = 100.0;
+    errCoeff(4,4) =   3.0;
+    errCoeff(5,5) =  25.0;
+    errCoeff(6,6) =   1.0;
+  // Eigen::VectorXd mForceErr = mForces + errCoeff*error(dq, DOF);
+  Eigen::VectorXd mForceErr = mTauPred + errCoeff*error(dq, DOF);
+
+
+  double diff = (mForceErr - mTauPred).norm();
+  cout << "Norm error: " << diff << endl;
+
 
   // Apply the joint space forces to the robot
   mRobot->setForces(mForceErr);
 
-  // dataQ       << q.transpose() << endl;
-  // dataQref    << qref.transpose() << endl;
+  dataTime    << mTime << endl;
+  dataQ       << q.transpose() << endl;
+  dataQref    << qref.transpose() << endl;
   // dataQdot    << dq.transpose() << endl;
   // dataQdotdot << ddq.transpose() << endl;
   // dataTorque  << mForces.transpose() << endl;
-  // dataTime    << mTime << endl;
   // dataM       << M << endl;
   // dataCg      << Cg.transpose() << endl;
   // dataError   << (errCoeff*error(dq, DOF)).transpose() << endl;
 
   // Closing operation
   double T = 2*3.1416/wf;
-
-  if (mTime >= 1*T ) {
-    cout << "Time period met. Closing simulation ...";
-    // cout << "Time period met. Stopping data recording ...";
-    // dataQ.close();
-    // dataQref.close();
+  if (mTime >= 2*T ) {
+    // cout << "Time period met. Closing simulation ..." << endl;
+    cout << "Time period met. Stopping data recording ...";
+    dataTime.close();
+    dataQ.close();
+    dataQref.close();
     // dataQdot.close();
     // dataQdotdot.close();
     // dataTorque.close();
-    // dataTime.close();
     // dataM.close();
     // dataCg.close();
     // dataError.close();
-    // cout << "File handles closed!" << endl << endl << endl;
+    cout << "File handles closed!" << endl << endl << endl;
     exit (EXIT_FAILURE);
   }
 }
