8f94faa8e40ffd938a6064e6821a29770372ee54
mouhyemen <mouhyemen.khan@gmail.com>
2018-04-30 20:47:33 -0400

    getQueryKernel corrected, changed stack limit

--- trackError.m
+++ trackError.m
@@ -1,19 +1,20 @@
 clc; clear; close all;
 dof   = 7;
 
-q = tdfread('./data/dataQ.txt', '\t');
-q = q.dataQ;
+onlineQ = tdfread('./data/onlineDataQ.txt', '\t');
+onlineQ = onlineQ.onlineDataQ;
 
-qref = tdfread('./data/dataQref.txt', '\t');
-qref = qref.dataQref;
+onlineQref = tdfread('./data/onlineDataQref.txt', '\t');
+onlineQref = onlineQref.onlineDataQref;
 
-time = tdfread('./data/dataTime.txt');
-time = time.dataTime;
+time = tdfread('./data/onlineDataTime.txt');
+time = time.onlineDataTime;
 
+%%
 rows = 3; cols = 3;
 for i = 1:dof
     subplot(rows, cols, i);
-    plot(time, q(:,i), time, qref(:,i)); grid on;
+    plot(time, onlineQ(:,i), 'r', time, onlineQref(:,i) ,'g'); grid on;
     xlabel('Time');
     ylabel('State Trajectories');
     leg1 = legend('$q(t)$','$q_{ref}(t)$');
@@ -21,3 +22,23 @@ for i = 1:dof
     xlabel(['Joint ' num2str(i)])
 end
 title('Tracking Error');
+
+% 
+% q = tdfread('./data/dataQ.txt', '\t');
+% q = q.dataQ;
+% 
+% qref = tdfread('./data/dataQref.txt', '\t');
+% qref = qref.dataQref;
+% 
+% %%
+% figure();
+% for i = 1:dof
+%     subplot(rows, cols, i);
+%     plot(time, q(:,i), time, qref(:,i)); grid on;
+%     xlabel('Time');
+%     ylabel('State Trajectories');
+%     leg1 = legend('$q(t)$','$q_{ref}(t)$');
+%     set(leg1,'Interpreter','latex');
+%     xlabel(['Joint ' num2str(i)])
+% end
+% title('Tracking Error');
