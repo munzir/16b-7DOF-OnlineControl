1abd999f6f41855d7a611eb1edae05ad0970720b
mouhyemen <mouhyemen.khan@gmail.com>
2018-06-05 13:10:14 -0400

    DART scripts (c++), data for plotting

--- Controller.hpp
+++ Controller.hpp
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <dart/dart.hpp>
 using namespace std;
-#define SAMPLES 2000
+#define SAMPLES 2174
 #define DOF 7
 #define DOF3 21
 
@@ -37,27 +37,42 @@ public:
 
 private:
   /// File handles for dumping online-data and reading offline data
-  ofstream onlineDataTime;
-  ofstream onlineDataQ;
-  ofstream onlineDataQref;
+  // States - q and qref
+  ofstream onlineTime;
+  ofstream onlineQ_ref;
+  ofstream onlineQ_rbd;
+  ofstream onlineQ_pred;
+  ofstream onlinexq;
+  // End-effector position
+  ofstream onlineEE_ref;
+  ofstream onlineEE_rbd;
+  ofstream onlineEE_pred;
+  // Torques
+  ofstream onlineTau_ref;
+  ofstream onlineTau_rbd;
+  ofstream onlineTau_pred;
+  ofstream onlinekAlpha;
+  ofstream onlineTau_non1;
+  ofstream onlineTau_non2;
 
   ifstream readAlpha;
-  ifstream readQ;
-  ifstream readQdot;
-  ifstream readQdotdot;
+  ifstream readTrainQ;
+  ifstream readTrainQdot;
+  ifstream readTrainQdotdot;
 
-  const int LINES = 1006;
-  Eigen::Matrix<double, SAMPLES, DOF3> xp;
+  // const int LINES = 1006;
+  Eigen::Matrix<double, SAMPLES, DOF*2> xp;
   Eigen::Matrix<double, SAMPLES, DOF> alpha;
 
   /// Variables for calculating reference (optical) trajectories
   double mTime;
-  double dt;
-  double wf;
-  Eigen::Matrix<double, DOF, 1> qref;
-  Eigen::Matrix<double, DOF, 1> dqref;
-  Eigen::Matrix<double, DOF, 1> q0;
-  Eigen::Matrix<double, DOF, 4> a, b;
+    int count = 0;
+    double dt;
+    double wf;
+    Eigen::Matrix<double, DOF, 1> qref;
+    Eigen::Matrix<double, DOF, 1> dqref;
+    Eigen::Matrix<double, DOF, 1> q0;
+    Eigen::Matrix<double, DOF, 4> a, b;
 
   /// \brief Robot
   dart::dynamics::SkeletonPtr mRobot;
@@ -65,9 +80,15 @@ private:
   /// \brief End-effector of the robot
   dart::dynamics::BodyNode* mEndEffector;
 
+  /// Variables for data generation
+  int flag = 1;
+
   /// \brief Control forces
-  Eigen::VectorXd mForces;
+  Eigen::VectorXd mTauRef;
+  Eigen::VectorXd mTauRbd;
   Eigen::VectorXd mTauPred;
+  Eigen::VectorXd kAlpha;
+  Eigen::VectorXd mForceNon;
 
   /// \brief Proportional gain for the virtual spring forces at the end effector
   Eigen::Matrix<double,DOF,DOF> mKp;
--- Main.cpp
+++ Main.cpp
@@ -1,34 +1,3 @@
-/*
- * Copyright (c) 2014-2016, Humanoid Lab, Georgia Tech Research Corporation
- * Copyright (c) 2014-2017, Graphics Lab, Georgia Tech Research Corporation
- * Copyright (c) 2016-2017, Personal Robotics Lab, Carnegie Mellon University
- * All rights reserved.
- *
- * This file is provided under the following "BSD-style" License:
- *   Redistribution and use in source and binary forms, with or
- *   without modification, are permitted provided that the following
- *   conditions are met:
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
- *   CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
- *   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- *   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- *   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
- *   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- *   USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- *   AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *   POSSIBILITY OF SUCH DAMAGE.
- */
-
 #include <dart/dart.hpp>
 #include <dart/utils/urdf/urdf.hpp>
 
@@ -45,7 +14,7 @@ int main(int argc, char* argv[])
   dart::dynamics::SkeletonPtr ground
       = dl.parseSkeleton("dart://sample/urdf/KR5/ground.urdf");
   dart::dynamics::SkeletonPtr robot
-      = dl.parseSkeleton("/home/mouhyemen/desktop/research/KrangDart/SingleArmQP/krangArm.urdf");
+      = dl.parseSkeleton("/home/mouhyemen/desktop/research/KrangDart/OnlineControl/krangArm.urdf");
   world->addSkeleton(ground);
   world->addSkeleton(robot);
 
